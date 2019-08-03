5d5509956fc385bd85b26e62a41720302ddcfaea
ARDA-ASUS\ardak <arda.kaplan09@gmail.com>
2019-08-03 15:11:17 +0300

    rdabroadcastrecevier

---
+++ RDALibrary/src/main/java/com/ardakaplan/rdalibrary/base/RDABroadcastReceiver.java
@@ -0,0 +1,21 @@
+package com.ardakaplan.rdalibrary.base;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import dagger.android.AndroidInjection;
+
+/**
+ * Created by Arda Kaplan at 02-Aug-19
+ * <p>
+ * arda.kaplan09@gmail.com
+ */
+public abstract class RDABroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        AndroidInjection.inject(this, context);
+    }
+}
