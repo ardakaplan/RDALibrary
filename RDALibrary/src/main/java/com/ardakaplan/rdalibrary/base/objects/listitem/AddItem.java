a13859f23a515c1b9e226946ca48377a9dd29973
ARDA-ASUS\ardak <arda.kaplan09@gmail.com>
2019-08-29 10:26:57 +0300

    multi item list object

---
+++ RDALibrary/src/main/java/com/ardakaplan/rdalibrary/base/objects/listitem/AddItem.java
@@ -0,0 +1,18 @@
+package com.ardakaplan.rdalibrary.base.objects.listitem;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Created by Arda Kaplan at 22-Aug-19
+ * <p>
+ * arda.kaplan09@gmail.com
+ */
+@Singleton
+public class AddItem implements MultiItemListObject {
+
+    @Inject
+    AddItem() {
+
+    }
+}
---
+++ RDALibrary/src/main/java/com/ardakaplan/rdalibrary/base/objects/listitem/MultiItemListObject.java
@@ -0,0 +1,9 @@
+package com.ardakaplan.rdalibrary.base.objects.listitem;
+
+/**
+ * Created by Arda Kaplan at 22-Aug-19
+ * <p>
+ * arda.kaplan09@gmail.com
+ */
+public interface MultiItemListObject {
+}
