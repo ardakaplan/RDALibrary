c981cb70862532a9b66fddd3bd284d8ae1bb3565
ARDA-ASUS\ardak <arda.kaplan09@gmail.com>
2020-08-11 21:09:14 +0300

    RDAWidgetProvider

--- RDALibrary/src/main/java/com/ardakaplan/rdalibrary/base/receivers/RDAWidgetProvider.java
+++ RDALibrary/src/main/java/com/ardakaplan/rdalibrary/base/receivers/RDAWidgetProvider.java
@@ -1,8 +1,10 @@
 package com.ardakaplan.rdalibrary.base.receivers;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 
 import dagger.android.AndroidInjection;
 
@@ -11,11 +13,23 @@ import dagger.android.AndroidInjection;
  * <p>
  * arda.kaplan09@gmail.com
  */
-public class RDAWidgetProvider extends AppWidgetProvider {
+public abstract class RDAWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         AndroidInjection.inject(this, context);
         super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        AndroidInjection.inject(this, context);
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        AndroidInjection.inject(this, context);
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
     }
 }
